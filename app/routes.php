c391a90a1544e861e5fc192d40cafe1ab78ca532
Jef Furlong <jef.furlong@gmail.com>
2013-06-26 08:01:21 -0700

    routing

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,12 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+Route::group(array('domain' => '{org}.'.Config::get('app.domain')), function()
+{
+    Route::get('/', function($org){
+        return View::make('org/home');
+    });
+});
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return View::make('main/home');
 });
