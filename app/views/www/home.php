214a157eaa9d5c006f0490585825fe9874bff6f2
Jef Furlong <jef.furlong@gmail.com>
2013-07-17 06:58:14 -0700

    17-jul-2013

--- app/views/www/home.php
+++ app/views/www/home.php
@@ -9,36 +9,43 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-lg-5"> <h2><i class="icon-heart"></i> Simple</h2>
-      <p class="text-large">MVP Registration is the easiest way to get paid. Our web app enables
-        you to accept credit cards and manage your registrants quickly and
-        easily. There's no software to install or complicated set-up process.
-        MVP Registration is ready to use as soon as your account is created.</p></div>
-        <div class="col-lg-6 col-offset-1"><img src="http://placehold.it/460x350" /></div>
-    </div>
-    <div class="row">
-        <div class="col-lg-6"><img src="http://placehold.it/460x350" /></div>
-        <div class="col-lg-5 col-offset-1">
-            <h2><i class="icon-cog"></i> Usable</h2>
-            <p>Are you frustrated by confusing software and complicated processes? MVP
-                Registration is a simple and elegant web app that works the way you
-                want. You'll appreciate the difference right away with a clean modern
-                design and a responsive, user-friendly experience.</p>
+<div class="benefits">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5"> <h2><i class="icon-heart"></i> Simple</h2>
+            <p>MVP Registration is the easiest way to get paid. Our web app enables
+            you to accept credit cards and manage your registrants quickly and
+            easily. There's no software to install or complicated set-up process.
+            MVP Registration is ready to use as soon as your account is created.</p></div>
+            <div class="col-lg-6 col-offset-1"><img src="http://placehold.it/460x350" /></div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-5">
-            <h2><i class="icon-lock"></i> Reliable</h2>
-            <p>With MVP Registration, you get worry-free reliability. We run frequent
-            multi-site backups, lock everything down with SSL, and pride ourselves
-            on highly responsive customer support. We ensure safety for you and your
-            customers with fully secure, industry-standard security practices.</p>
+
+        <hr />
+
+        <div class="row">
+            <div class="col-lg-6"><img src="http://placehold.it/460x350" /></div>
+            <div class="col-lg-5 col-offset-1">
+                <h2><i class="icon-cog"></i> Usable</h2>
+                <p>Are you frustrated by confusing software and complicated processes? MVP
+                    Registration is a simple and elegant web app that works the way you
+                    want. You'll appreciate the difference right away with a clean modern
+                    design and a responsive, user-friendly experience.</p>
+            </div>
         </div>
-        <div class="col-lg-6 col-offset-1"><img src="http://placehold.it/460x350" /></div>
+
+        <hr />
+
+        <div class="row">
+            <div class="col-lg-5">
+                <h2><i class="icon-lock"></i> Reliable</h2>
+                <p>With MVP Registration, you get worry-free reliability. We run frequent
+                multi-site backups, lock everything down with SSL, and pride ourselves
+                on highly responsive customer support. We ensure safety for you and your
+                customers with fully secure, industry-standard security practices.</p>
+            </div>
+            <div class="col-lg-6 col-offset-1"><img src="http://placehold.it/460x350" /></div>
+        </div>
     </div>
 </div>
-
 <?php include 'partials/footer.php'; ?>
 
