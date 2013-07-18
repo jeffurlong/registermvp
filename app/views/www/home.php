b8714603ce20c853c8c7957df0d0c9a8d715cebd
Jef Furlong <jef.furlong@gmail.com>
2013-07-18 09:38:11 -0700

    18-jul-2013

--- app/views/www/home.php
+++ app/views/www/home.php
@@ -1,7 +1,6 @@
 <?php include 'partials/header.php'; ?>
 
 <div class="hero">
-    <div class="mask"></div>
     <div class="container">
         <h1>Online Registration Made Simple</h1>
         <p class="lead">A refreshingly simple approach to accepting and managing online registrations.</p>
